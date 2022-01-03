705256678d6789fa388a84896d699785d64f69ab
Everett Flynn <44507725+OutmodedChip64@users.noreply.github.com>
2022-01-02 21:31:58 -0800

    Convert to PWA (probably)
    * Created ability to install (probably)

---
+++ public/serviceWorker.js
@@ -0,0 +1,48 @@
+var CACHE_NAME = 'strawberry-v0.2-cache';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['strawberry-v0.2-cache'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
